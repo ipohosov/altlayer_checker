61773bfbff3047882a22154f1debad889b223362
ipohosov <pogosovivan@gmail.com>
2024-01-25 00:32:00 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+**1. python 3.8+ should be installed**
+
+**2. Execute commands:**
+    
+pip install -r requirements.txt
+
+cp example.env .env
+
+**3. Fill .env file via your wallet addresses (Not use private keys just WALLET ADDRESSES)**
+
+**4. Execute script:**
+
+python drop_check.py
+
+
+Say thank you ***0x5f11D23968Dd31eD3358B2eD052A37F2716FBA65***
---
+++ drop_check.py
@@ -0,0 +1,71 @@
+import re
+
+import requests
+from prettytable import PrettyTable
+
+
+def read_file():
+    with open(".env", 'r') as file:
+        data = [line.strip() for line in file if not line.startswith("#")]
+    return data
+
+
+def get_drop_data(wallet_address) -> dict:
+    response = None
+    result_dict = []
+    url = "https://94c87e96-000a-4740-bc01-0ca9abbb7bca-stg-airdrop.alt.technology/"
+
+    payload = f'["{wallet_address}"]'
+    headers = {
+        'content-type': 'text/plain;charset=UTF-8',
+        'next-action': '67fc7bba70928774c5305f604afe585929202b77'
+    }
+    response_code = 0
+    while response_code != 200:
+        response = requests.request("POST", url, headers=headers, data=payload)
+        response_code = response.status_code
+
+    # Extracting information using regular expressions
+    pattern = re.compile(
+        r'address\":\"(?P<address>0x\w+)\",\"amount\":\"(?P<amount>\d+)\".+,'
+        r'"og":(?P<og>\w+),"ottie":(?P<ottie>\w+),"altitude":(?P<altitude>\w+),'
+        r'"eigenRestaker":(?P<eigenRestaker>\w+),"eigenEcosystem":(?P<eigenEcosystem>\w+)}')
+
+    match = pattern.search(response.text)
+
+    if match:
+        result_dict = match.groupdict()
+        result_dict["amount"] = int(result_dict["amount"])/1000000000000000000
+    else:
+        result_dict = {
+            "address": wallet_address,
+            "amount": 0,
+            "og": False,
+            "ottie": False,
+            "altitude": False,
+            "eigenRestaker": False,
+            "eigenEcosystem": False
+        }
+
+    return result_dict
+
+
+credits = []
+wallet_addresses = read_file()
+progress = 0
+progress_delta = 100 / len(wallet_addresses)
+print(f"Current progress - {progress}%")
+for address in wallet_addresses:
+    credits.append(get_drop_data(address))
+    progress += progress_delta
+    print(f"Current progress - {round(progress, 2)}%")
+
+sorted_credits = sorted(credits, key=lambda d: d['amount'], reverse=True)
+
+table = PrettyTable(['address', 'amount', 'og', 'ottie', 'altitude', 'eigenRestaker', 'eigenEcosystem'])
+table.align = "l"
+
+for credit in sorted_credits:
+    table.add_row([credit.get("address"), credit.get("amount"), credit.get("og"), credit.get("ottie"),
+                   credit.get("altitude"), credit.get("eigenRestaker"), credit.get("eigenEcosystem")])
+print(table)
